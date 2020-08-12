3bd6d2dfb61bca603144538c66ada64bfed31c78
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-12 14:54:32 +0200

    fix append to check the return value of check capacity function

--- lib/my_string/append.c
+++ lib/my_string/append.c
@@ -15,7 +15,8 @@ int my_string_append(my_string *dest, wchar_t *src, long n) {
   if (!n || n > src_len)
     n = src_len;
 
-  my_string_check_capacity(dest, dest->size + n);
+  if (my_string_check_capacity(dest, dest->size + n) < 0)
+    return -1;
 
   for (long i = 0; i < n; i++)
     dest->unicode[dest->size + i] = src[i];
